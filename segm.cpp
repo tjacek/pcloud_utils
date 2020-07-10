ceb1f10e5a99dc303ea02d4d091ee2aeb4a86bde
tjacek <tjacek@agh.edu.pl>
2020-07-10 13:27:57 +0200

    min cut segmentation

--- segm.cpp
+++ segm.cpp
@@ -1,7 +1,8 @@
 #include "segm.h"
 
 void pcloud_segmentation(PCloud & pcloud,std::string seq_path){
-  std::vector<pcl::PointIndices> clust=growth_segmentation(pcloud);
+//  std::vector<pcl::PointIndices> clust=growth_segmentation(pcloud);
+  std::vector<pcl::PointIndices> clust=min_cut(pcloud);
   std::list<cv::Mat> new_frames;
   for(auto it = clust.begin(); it!=clust.end(); ++it){
     pcl::PointIndices ind_i=(*it);
@@ -21,8 +22,7 @@ std::list<PCloud> simple_segm(std::list<PCloud> & pclouds){
   for(auto it = pclouds.begin(); it!=pclouds.end(); ++it){
     PCloud pcloud_i=(*it);
     std::vector <pcl::PointIndices> clust_i=growth_segmentation(pcloud_i);
-    pcl::PointIndices first=clust_i.front();//= pcl::PointIndices::Ptr(&clust_i.front());
-//    pcl::PointIndices::Ptr first=pcl::PointIndices::Ptr(new pcl::PointIndicess);
+    pcl::PointIndices first=clust_i.front();
     new_pclouds.push_back( extract_cloud(first ,pcloud_i));
   }
   return new_pclouds;
@@ -35,14 +35,14 @@ std::vector <pcl::PointIndices> growth_segmentation(PCloud & cloud){
   pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator;
   normal_estimator.setSearchMethod (tree);
   normal_estimator.setInputCloud (cloud);
-  normal_estimator.setKSearch (50);
-  normal_estimator.compute (*normals);
+  normal_estimator.setKSearch(20); //(50);
+  normal_estimator.compute(*normals);
 
   pcl::IndicesPtr indices (new std::vector <int>);
   pcl::PassThrough<pcl::PointXYZ> pass;
-  pass.setInputCloud (cloud);
-  pass.setFilterFieldName ("z");
-  pass.setFilterLimits (0.0, 1.0);
+  pass.setInputCloud(cloud);
+  pass.setFilterFieldName("z");
+  pass.setFilterLimits(0.0,1.0);
   pass.filter (*indices);
 
   pcl::RegionGrowing<pcl::PointXYZ, pcl::Normal> reg;
@@ -58,5 +58,42 @@ std::vector <pcl::PointIndices> growth_segmentation(PCloud & cloud){
 
   std::vector <pcl::PointIndices> clusters;
   reg.extract (clusters);
+  return clusters;
+}
+
+std::vector <pcl::PointIndices> min_cut(PCloud & cloud){
+  pcl::IndicesPtr indices (new std::vector <int>);
+  pcl::PassThrough<pcl::PointXYZ> pass;
+  pass.setInputCloud(cloud);
+  //pass.setFilterFieldName("z");
+  //pass.setFilterLimits(0.0,1.0);
+  pass.filter (*indices);
+  
+
+  pcl::MinCutSegmentation<pcl::PointXYZ> seg;
+  seg.setInputCloud (cloud);
+  seg.setIndices (indices);
+  
+  Eigen::Vector4f centroid;
+  pcl::compute3DCentroid(*cloud, centroid);
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr foreground_points(new pcl::PointCloud<pcl::PointXYZ> ());
+  pcl::PointXYZ point;
+  point.x = centroid[0];//120;
+  point.y = centroid[1];//160;
+  point.z = centroid[2];//0.5;
+  foreground_points->points.push_back(point);
+  seg.setForegroundPoints (foreground_points);
+
+  seg.setSigma (1.0);
+  seg.setRadius (32);
+  seg.setNumberOfNeighbours( 14); //(14);
+  seg.setSourceWeight (0.2);
+
+  std::vector <pcl::PointIndices> clusters;
+  seg.extract(clusters);
+  cout << clusters[0].indices.size() << endl;
+  cout << clusters[1].indices.size() << endl;
+
   return clusters;
 }
--- segm.h
+++ segm.h
@@ -4,7 +4,9 @@
 #include <pcl/search/kdtree.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl/features/normal_3d.h>
+#include <pcl/segmentation/min_cut_segmentation.h>
 
 std::list<PCloud> simple_segm(std::list<PCloud> & pclouds);
-std::vector <pcl::PointIndices> growth_segmentation(PCloud & cloud);
 void pcloud_segmentation(PCloud & pcloud,std::string seq_path);
+std::vector <pcl::PointIndices> growth_segmentation(PCloud & cloud);
+std::vector <pcl::PointIndices> min_cut(PCloud & cloud);
