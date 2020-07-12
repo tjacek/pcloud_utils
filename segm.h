4126771b91c3a0aa3970ba8476a63515f2f44f28
tjacek <tjacek@agh.edu.pl>
2020-07-12 16:30:04 +0200

    diff_of_normal

--- segm.h
+++ segm.h
@@ -6,7 +6,12 @@
 #include <pcl/features/normal_3d.h>
 #include <pcl/segmentation/min_cut_segmentation.h>
 
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/features/don.h>
+
 std::list<PCloud> simple_segm(std::list<PCloud> & pclouds);
 void pcloud_segmentation(PCloud & pcloud,std::string seq_path);
 std::vector <pcl::PointIndices> growth_segmentation(PCloud & cloud);
 std::vector <pcl::PointIndices> min_cut(PCloud & cloud);
+std::vector <pcl::PointIndices> diff_of_normal(PCloud & cloud);
