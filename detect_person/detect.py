aaca71e34e7a9c8b4f59f65641f62fa549e6fa55
tjacek <tjacek@agh.edu.pl>
2020-07-13 13:31:09 +0200

    experiment

--- detect_person/detect.py
+++ detect_person/detect.py
@@ -3,8 +3,15 @@ import keras,cv2
 import data,preproc,cnn
 from keras.models import load_model
 
+def exp(in_path):
+    dirs=["dataset","nn","segm","floor"]
+    paths={ dir_i:"%s/%s"%(in_path,dir_i) for dir_i in dirs}
+#    train_model(paths["dataset"],paths["nn"],n_epochs=1000)
+    get_persons(paths["segm"],paths["nn"],paths["floor"])
+
 def get_persons(in_path,nn_path,out_path):
     clust_dict=data.read_clusters(in_path)
+    print(clust_dict)
     preproc_dict=preproc.preproc_clusters(clust_dict)
     model=load_model(nn_path)
     data.make_dir(out_path)
@@ -15,8 +22,9 @@ def get_persons(in_path,nn_path,out_path):
             X_i=np.array(segm_j)
             X_i=np.expand_dims(X_i,axis=-1)
             y_true=model.predict(X_i)
+            print(np.argmax(y_true,axis=1))
             y_true=np.nonzero(np.argmax(y_true,axis=1))[0]
-            if(y_true.shape[0]==1):
+            if(y_true.shape[0]>0):
                 person_segm_j=np.squeeze(X_i[y_true[0]])
                 out_ij="%s/%s.png"%(cat_path,frame_j)
                 cv2.imwrite(out_ij,person_segm_j) 
@@ -25,7 +33,7 @@ def train_model(in_path,out_path=None,n_epochs=100):
     raw_dict=data.read_dataset(in_path)
     preproc_dict=preproc.preproc_dict(raw_dict)
     X_pos,X_neg=preproc_dict['pos'],preproc_dict['neg']
-    y_pos,y_neg=np.repeat(0,len(X_pos)),np.repeat(1,len(X_neg))
+    y_pos,y_neg=np.repeat(1,len(X_pos)),np.repeat(0,len(X_neg))
     X=np.array(X_pos+X_neg)
     y= keras.utils.to_categorical(np.concatenate([y_pos,y_neg]))
     X=np.expand_dims(X,axis=-1)
@@ -34,5 +42,4 @@ def train_model(in_path,out_path=None,n_epochs=100):
     if(out_path):
         model.save(out_path)
 
-#train_model("dataset","nn",n_epochs=1000)
-get_persons("../segm","nn","result")
+exp("../growth")
--- detect_person/img_segm.py
+++ detect_person/img_segm.py
@@ -1,32 +1,34 @@
 import cv2,numpy as np
 import data
 
-def segm(in_path,out_path):
+def segm(in_path,out_path,fun=None):
+    if(not fun):
+        fun=clust_segm
     seq_dict=data.read_seqs(in_path)
-    print( type(seq_dict['A'][0]))
-    cls_dict={ name_i:[proc_img(frame_j) 
+    cls_dict={ name_i:[fun(frame_j) 
                 for frame_j in seq_i]
                     for name_i,seq_i in seq_dict.items()}
-    print(cls_dict.keys())
     data.save_clusters(cls_dict,out_path)
 
-def proc_img(img_i,k=4):
+def clust_segm(img_i,k=4):
     img_i = cv2.cvtColor(img_i, cv2.COLOR_GRAY2RGB)
     pixel_values = img_i.reshape((-1, 3))
     pixel_values = np.float32(pixel_values)
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
-    _, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)    
-    centers = np.uint8(centers)
+    _, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
     labels = labels.flatten()
-    segmented_image = centers[labels]
-    segmented_image = segmented_image.reshape(img_i.shape)
-#    cls_i=[cv2.cvtColor(segmented_image, cv2.COLOR_BGR2GRAY)]
-    
-    masked_img=np.copy(img_i)
-    masked_img=masked_img.reshape((-1,3))
-    masked_img[labels==1]=[0,0,0]
-    masked_img=masked_img.reshape(img_i.shape)    
-    return [masked_img]
+    def helper(i):
+        masked_img=np.copy(img_i)
+        masked_img=masked_img.reshape((-1,3))
+        masked_img[labels!=i]=[0,0,0]
+        masked_img=masked_img.reshape(img_i.shape)
+        return masked_img    
+    return [ helper(i) for i in range(k)]
 
-in_path="../normal/result"
-segm(in_path,"../normal/cls")
+def flood_segm(img_i):
+    edges = cv2.Canny(img_i,10,200)
+    return [edges]
+
+if __name__=="__main__":
+    in_path="../normal/result"
+    segm(in_path,"../normal/cls",flood_segm)
