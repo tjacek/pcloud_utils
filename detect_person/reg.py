6abb7dbcdf91cc14dbe1f78362d5d6f01a875d5a
tjacek <tjacek@agh.edu.pl>
2020-07-14 13:51:40 +0200

    test regression dataset

--- detect_person/reg.py
+++ detect_person/reg.py
@@ -9,7 +9,7 @@ def reg_dataset(in_path,out_path):
         for j,frame_j in enumerate(frames):
             y=aprox_floor(frame_j)
             if(y):
-               img_id="%s/%d"%(cat_i,j)
+               img_id="%s/%s/%d.png"%(in_path,cat_i,j)
                dataset[img_id]=y
     save_dict(dataset,out_path)
 
@@ -23,6 +23,21 @@ def aprox_floor(img_i):
         return lines[0]
     return lines[1][np.argmax(lines[0])]
 
+def cut_floor(in_path,out_path):
+    reg_dict=read_dict(in_path)
+    data.make_dir(out_path)
+    for path_i,reg_i in reg_dict.items():
+        img_i=cv2.imread(path_i, cv2.IMREAD_GRAYSCALE)
+        out_i="%s/%s.png" % (out_path,path_i.split('/')[-1])
+        img_i[int(reg_i):,:]=0
+        cv2.imwrite(out_i,img_i)
+
+def read_dict(in_path):
+    with open(in_path, mode='r') as infile:
+        reader=csv.reader(infile)
+        dataset={rows[0]:rows[1] for rows in reader}
+    return dataset
+
 def save_dict(reg_dict,out_path):
     with open(out_path, 'w') as f:  
         w = csv.writer(f)
@@ -30,4 +45,6 @@ def save_dict(reg_dict,out_path):
 
 in_path="../growth/imgs/result"
 out_path="test"
+
 reg_dataset(in_path,out_path)
+cut_floor("test","out")
