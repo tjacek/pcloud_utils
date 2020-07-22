21c9315091527741e553ec54f01663bbfa010779
tjacek <tjacek@agh.edu.pl>
2020-07-22 18:52:39 +0200

    bound person

--- detect_person/bound.py
+++ detect_person/bound.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.ndimage import gaussian_filter1d
 import data
 
 def aprox_bound(in_path,out_path):
@@ -7,7 +8,25 @@ def aprox_bound(in_path,out_path):
 def fun(img_i):
     img_i[img_i!=0]=1
     ts_i=np.mean(img_i,axis=0)
-    return bar_plot(ts_i)
+    ts_i=gaussian_filter1d(ts_i, 6)    
+#    max_ts=np.argmax(ts_i)
+    x0,x1=get_inflected(ts_i)
+    plot_i=bar_plot(ts_i)
+    plot_i[:,:x0]=0
+    plot_i[:,x1:]=0
+    return plot_i
+
+def get_inflected(ts_i):
+    max_ts=np.argmax(ts_i)
+    diff_i=np.diff(ts_i)
+    extr_i= np.abs( np.diff(np.sign(diff_i)))
+    extr_i=np.where(extr_i==2)
+    if(extr_i[0].shape[0]==0):
+        return 0,ts_i.shape[0]
+    extr_i=extr_i[0]
+    left=np.argmax( extr_i>max_ts)
+    x0,x1= extr_i[left-2],extr_i[left] 
+    return x0,x1
 
 def bar_plot(ts_i):
     plot_i=np.zeros((100,ts_i.shape[0]))
@@ -15,6 +34,5 @@ def bar_plot(ts_i):
         k=int( (1.0-value_j) *100)
         plot_i[k:,j]=100
     return plot_i
-
 
 aprox_bound("raw","test")
