c8e103ea49ccd8da2f873c772c106b4504179bb4
tjacek <tjacek@agh.edu.pl>
2020-07-26 18:06:26 +0200

    format names

--- tools/get_rep.py
+++ tools/get_rep.py
@@ -1,4 +1,23 @@
-import os,shutil
+import re,os,shutil
+
+def format_names(in_path,out_path):
+    paths=os.listdir(in_path)
+    persons=list(unique_person(paths))
+    persons.sort()
+    size=len(persons)/2
+    def helper(i):
+        return 2*(i-size) if(i>size) else (2*i+1)
+    persons={ person_i: int(helper(i)) 
+                for i,person_i in enumerate(persons)}
+    make_dir(out_path)
+    for path_i in paths:
+        raw=path_i.split("_")
+        person_i= persons[raw[-2].lower()]
+        cat_i=re.sub('[^0-9]','',raw[-1])
+        name_i="%s_%s_1"%(cat_i, person_i)
+        in_i="%s/%s"%(in_path,path_i)
+        out_i="%s/%s"%(out_path,name_i)
+        shutil.copytree(in_i, out_i)
 
 def person_rep(in_path,out_path):
     paths=os.listdir(in_path)
@@ -8,18 +27,18 @@ def person_rep(in_path,out_path):
         if(not person_i in person_paths):
             full_path_i="%s/%s" % (in_path,path_i)
             person_paths[person_i]=full_path_i
-#    unique_person(paths)
     make_dir(out_path)
     for name_i,path_i in person_paths.items():	
         print(path_i)
         out_i="%s/%s"%(out_path,path_i.split('/')[-1])
         print(out_i)
         shutil.copytree(path_i, out_i)
-#def unique_person(paths):
-#    ids=set()
-#    for path_i in paths:
-#        ids.update([get_person(path_i)]) 
-#    return ids	
+
+def unique_person(paths):
+    ids=set()
+    for path_i in paths:
+        ids.update([get_person(path_i).lower()]) 
+    return ids	
 
 def get_person(path_i):
     return path_i.split("_")[-2]	
@@ -28,5 +47,5 @@ def make_dir(path):
     if(not os.path.isdir(path)):
         os.mkdir(path)
 
-in_path="../../clf/result"
-person_rep(in_path,"test")
+in_path="../detect_person/final"
+format_names(in_path,"test")
