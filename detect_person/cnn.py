8930fe26873b460c367fcdc53be29da69cdde9bf
tjacek <tjacek@student.agh.edu.pl>
2020-08-02 18:51:10 +0200

    large size network

--- detect_person/cnn.py
+++ detect_person/cnn.py
@@ -17,12 +17,25 @@ def basic_model(img_shape=(64,64,1),n_dense=32):
     x=Dropout(0.5)(x)
     return x,input_img
 
+def large_img_model(img_shape=(64,64,1),n_dense=64):
+    input_img = Input(shape=img_shape)
+    x=input_img
+    kern_size,pool_size,filters=(6,6),(4,4),[32,16,16,16]
+    for filtr_i in filters:
+        x = Conv2D(filtr_i, kern_size, activation='relu', padding='same')(x)
+        x = MaxPooling2D(pool_size, padding='same')(x)
+    x=Flatten()(x)
+    x=Dense(n_dense, activation='relu',name="hidden",kernel_regularizer=regularizers.l1(0.01),)(x)
+    x=Dropout(0.5)(x)
+    return x,input_img
+
 def make_model(img_shape=(64,64,1)):
-    x,input_img=basic_model(img_shape)
+    x,input_img=large_img_model(img_shape)
     x=Dense(units=2,activation='softmax')(x)
     model = Model(input_img, x)
     model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.SGD(lr=0.001,  momentum=0.9, nesterov=True))
+#              optimizer=keras.optimizers.SGD(lr=0.001,  momentum=0.9, nesterov=True))
+                optimizer=keras.optimizers.Adam(lr=0.001))
     model.summary()
     return model
 
--- detect_person/detect.py
+++ detect_person/detect.py
@@ -6,10 +6,10 @@ import numpy as np
 import keras,cv2
 import clf,reg,bound
 
-def clf_exp(in_path,out_path):
+def clf_exp(in_path,out_path,n_epochs=100):
     dirs=["dataset","nn","result"]
     paths={ dir_i:"%s/%s"%(out_path,dir_i) for dir_i in dirs}
-#    clf.train_model(paths["dataset"],paths["nn"],n_epochs=1000)
+    clf.train_model(paths["dataset"],paths["nn"],n_epochs=n_epochs)
     clf.get_persons(in_path,paths["nn"],paths["result"])
 
 def reg_exp(in_path,out_path):
