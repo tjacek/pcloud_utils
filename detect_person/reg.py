700bf03f56b5925ab642318415bb32705c3b0dc7
tjacek <tjacek@agh.edu.pl>
2020-08-21 13:23:26 +0200

    refactore reg

--- detect_person/reg.py
+++ detect_person/reg.py
@@ -1,9 +1,25 @@
-#import cv2
-#import numpy as np
-#from keras.models import load_model
-#from scipy.ndimage import gaussian_filter1d
-#import frames,cnn,clf
-import gui,dataset
+import numpy as np
+import frames,gui,dataset,cnn
+from keras.models import load_model
+
+def train_reg(in_path,out_path,n_epochs=1000):
+    reg_dict=dataset.read_dict(in_path)
+    X,y=dataset.train_dataset(reg_dict)
+    img_shape=(X.shape[1],X.shape[2],1)
+    model=cnn.make_regression(img_shape,2)
+    model.fit(X,y,epochs=n_epochs,batch_size=16)
+    if(out_path):
+        model.save(out_path)
+
+def apply_reg(in_path,nn_path,out_path):
+    model=load_model(nn_path)
+    def helper(frame_i):
+        view_i= np.expand_dims(frame_i,axis=-1)
+        view_i= np.expand_dims(view_i,axis=0)
+        r_i=model.predict(view_i)        
+        position=int(r_i[0][0]),int(r_i[0][1])
+        return simple_cut(frame_i,position) #frame_i
+    frames.transform_template(in_path,out_path,helper)
 
 def exp(in_path,out_path,k=100):
     fun=gui_gen
