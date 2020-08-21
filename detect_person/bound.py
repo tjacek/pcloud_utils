fcb24d98bcbb76c3b4c25a0bb4838108dde3115f
tjacek <tjacek@agh.edu.pl>
2020-08-21 14:09:47 +0200

    refactor bound

--- detect_person/bound.py
+++ detect_person/bound.py
@@ -1,9 +1,25 @@
-#import numpy as np,os.path
-#from scipy.ndimage import gaussian_filter1d
-#from keras.models import load_model
-#import dataset,frames,cnn,gui
-#import cv2
-import gui,dataset
+import numpy as np
+import gui,dataset,cnn,frames
+from keras.models import load_model
+
+def train_reg(in_path,out_path,n_epochs=1000):
+    reg_dict=dataset.read_dict(in_path)
+    X,y=dataset.train_dataset(reg_dict)
+    img_shape=(X.shape[1],X.shape[2],1)
+    model=cnn.make_regression(img_shape,4)
+    model.fit(X,y,epochs=n_epochs,batch_size=16)
+    if(out_path):
+        model.save(out_path)
+
+def apply_reg(in_path,nn_path,out_path):
+    model=load_model(nn_path)
+    def helper(frame_i):
+        view_i= np.expand_dims(frame_i,axis=-1)
+        view_i= np.expand_dims(view_i,axis=0)
+        r_i=model.predict(view_i)        
+        position=[int(r_i[0][i]) for i in range(4)]
+        return rect_cut(frame_i,position) #frame_i
+    frames.transform_template(in_path,out_path,helper)
 
 def exp(in_path,out_path,k=100):
     fun=gui_gen
@@ -28,28 +44,6 @@ def rect_cut(img_i,position):
     img_i[:y,:]=0
     img_i[y+height:,:]=0
     return img_i
-
-#def apply_reg(in_path,nn_path,out_path):
-#    model=load_model(nn_path)
-#    def helper(frame_i):
-#        frame_i=frame_i.reshape((1,frame_i.shape[0],frame_i.shape[1],1))
-#        r_i=model.predict(frame_i)
-#        r_i=model.predict(np.array([np.expand_dims(frame_i,axis=-1)]) )       
-#        x0,x1=int(r_i[0][0]),int(r_i[0][1])
-#        raise Exception(frame_i.shape)
-#        frame_i[:,:x0]=0
-#        frame_i[:,x1:]=0
-#        return frame_i
-#    frames.transform_template(in_path,out_path,helper)
-
-#def train_reg(in_path,out_path,n_epochs=1000):
-#    reg_dict=dataset.read_dict(in_path)
-#    X,y=dataset.train_dataset(reg_dict)
-#    img_shape=(X.shape[1],X.shape[2],1)
-#    model=cnn.make_regression(img_shape,4)
-#    model.fit(X,y,epochs=n_epochs,batch_size=16)
-#    if(out_path):
-#        model.save(out_path)
 
 if __name__=="__main__":
     in_path="final"#"../depth"
