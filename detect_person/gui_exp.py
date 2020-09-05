b69eed9cb34313394a18013c478fef7ca78fc2f7
tjacek <tjacek@agh.edu.pl>
2020-09-05 22:08:10 +0200

    show frame

--- detect_person/gui_exp.py
+++ detect_person/gui_exp.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtGui, QtCore, QtWidgets
-import cv2
-import sys
-import dataset
+import cv2,sys
+from ast import literal_eval 
+import dataset,foreground
 
 class ComboBoxDemo(QtWidgets.QWidget):
 
@@ -12,10 +12,11 @@ class ComboBoxDemo(QtWidgets.QWidget):
         self.comboBox = QtWidgets.QComboBox(self)
         self.comboBox.setGeometry(50, 50, 400, 35)
         self.comboBox.addItems(list(data_dict.keys()))
+        self.comboBox.currentTextChanged.connect(self.getComboValue)
 
         self.btn = QtWidgets.QPushButton('Show', self)
         self.btn.setGeometry(50, 150, 120, 35)
-        self.btn.clicked.connect(self.getComboValue)
+        self.btn.clicked.connect(self.show_frame)
 
         self.textbox = QtWidgets.QLineEdit(self)
         self.textbox.move(50, 90)
@@ -25,8 +26,13 @@ class ComboBoxDemo(QtWidgets.QWidget):
         frame_i=self.comboBox.currentText()
         value_i=self.data_dict[frame_i]
         self.textbox.setText(str(value_i))
-        print( dir(self.textbox))
-#        print((frame_i,self.data_dict[frame_i]))
+
+    def show_frame(self):
+        frame_i=self.comboBox.currentText()
+        position=literal_eval(self.textbox.text())
+        img_i=cv2.imread(frame_i, cv2.IMREAD_GRAYSCALE)
+        img_i=foreground.back_cut(img_i,position)
+        cv2.imshow(frame_i,img_i)
 
 in_path="test2/dataset"
 data_i=dataset.read_dict(in_path)
