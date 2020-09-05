f6e7cab35e6ab4fa921f2b15ba80964b771f4187
tjacek <tjacek@agh.edu.pl>
2020-09-05 21:06:11 +0200

    textbox

--- detect_person/gui_exp.py
+++ detect_person/gui_exp.py
@@ -13,13 +13,20 @@ class ComboBoxDemo(QtWidgets.QWidget):
         self.comboBox.setGeometry(50, 50, 400, 35)
         self.comboBox.addItems(list(data_dict.keys()))
 
-        self.btn = QtWidgets.QPushButton('Click', self)
-        self.btn.setGeometry(170, 120, 120, 35)
+        self.btn = QtWidgets.QPushButton('Show', self)
+        self.btn.setGeometry(50, 150, 120, 35)
         self.btn.clicked.connect(self.getComboValue)
+
+        self.textbox = QtWidgets.QLineEdit(self)
+        self.textbox.move(50, 90)
+        self.textbox.resize(280,40)
 
     def getComboValue(self):
         frame_i=self.comboBox.currentText()
-        print((frame_i,self.data_dict[frame_i]))#self.comboBox.currentIndex()))
+        value_i=self.data_dict[frame_i]
+        self.textbox.setText(str(value_i))
+        print( dir(self.textbox))
+#        print((frame_i,self.data_dict[frame_i]))
 
 in_path="test2/dataset"
 data_i=dataset.read_dict(in_path)
