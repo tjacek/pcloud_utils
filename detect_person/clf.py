330ffb4b8d48372839b2b8d1f2f7a78ef6176103
tjacek <tjacek@agh.edu.pl>
2020-10-16 14:39:29 +0200

    red clf dataset

--- detect_person/clf.py
+++ detect_person/clf.py
@@ -1,12 +1,12 @@
-#import numpy as np
+import numpy as np
 #import random,keras
 #from itertools import chain
 #from keras.models import load_model
 #import preproc,cnn
-import frames
+import frames,dataset
 
 def make_dataset(in_path,out_path):
-    paths=frames.get_dict(in_path)    
+    paths=frames.get_dirs(in_path)    
     seqs=[frames.get_files(path_i) for path_i in paths]
     pos,neg=[],[]
     for seq_i in seqs:
@@ -20,41 +20,55 @@ def make_dataset(in_path,out_path):
         frames.save_frames(frames_i,cat_path)
         print(cat_path)
 
+def read_clf_dataset(in_path):
+    dataset=frames.read_dataset(in_path)
+    X,y=[],[]
+    for i,cat_i in enumerate(dataset.keys()):
+        for frame_j in dataset[cat_i]:
+            print(frame_j.shape)
+            X.append(frame_j)
+            y.append(i)
+    return np.array(X),y
+
 def no_action_split(seq_i):
     neg=[seq_i[0],seq_i[-1]]
     center=int(len(seq_i)/2)
     pos=[seq_i[center]]
     return neg,pos
 
-def get_persons(in_path,nn_path,out_path,imgs_shape=None):
-    model=load_model(nn_path)
-    def helper(frames):
-        if(imgs_shape):
-            frames= preproc.standarize(frames,imgs_shape) 
-        X_i=np.array(frames)
-        X_i=np.expand_dims(X_i,axis=-1)
-        y_true=model.predict(X_i)
-        k=np.argmax(y_true[:,1])
-        person=np.squeeze(X_i[k] )
-        return person
-    data.cluster_template(in_path,out_path,helper)
-
 def train_model(in_path,out_path=None,n_epochs=100,imgs_shape=None):
-    raw_dict=data.read_dataset(in_path)
-    preproc_dict=preproc.preproc_dict(raw_dict) if(imgs_shape) else raw_dict
-    X_pos,X_neg=preproc_dict['pos'],preproc_dict['neg']
-    y_pos,y_neg=np.repeat(0,len(X_pos)),np.repeat(1,len(X_neg))
-    X=np.array(X_pos+X_neg)
-    y= keras.utils.to_categorical(np.concatenate([y_pos,y_neg]))
-    X=np.expand_dims(X,axis=-1)
-    imgs_shape=(imgs_shape[0],imgs_shape[1],1) if(imgs_shape) else (X.shape[1],X.shape[2],1) 
-    model=cnn.make_model(imgs_shape)
-    model.fit(X,y,epochs=n_epochs,batch_size=8)#64)
-    if(out_path):
-        model.save(out_path)
+    X,y=read_clf_dataset(in_path)
+    raise Exception( y)
+#def get_persons(in_path,nn_path,out_path,imgs_shape=None):
+#    model=load_model(nn_path)
+#    def helper(frames):
+#        if(imgs_shape):
+#            frames= preproc.standarize(frames,imgs_shape) 
+#        X_i=np.array(frames)
+#        X_i=np.expand_dims(X_i,axis=-1)
+#        y_true=model.predict(X_i)
+#        k=np.argmax(y_true[:,1])
+#        person=np.squeeze(X_i[k] )
+#        return person
+#    data.cluster_template(in_path,out_path,helper)
+
+#def train_model(in_path,out_path=None,n_epochs=100,imgs_shape=None):
+#    raw_dict=data.read_dataset(in_path)
+#    preproc_dict=preproc.preproc_dict(raw_dict) if(imgs_shape) else raw_dict
+#    X_pos,X_neg=preproc_dict['pos'],preproc_dict['neg']
+#    y_pos,y_neg=np.repeat(0,len(X_pos)),np.repeat(1,len(X_neg))
+#    X=np.array(X_pos+X_neg)
+#    y= keras.utils.to_categorical(np.concatenate([y_pos,y_neg]))
+#    X=np.expand_dims(X,axis=-1)
+#    imgs_shape=(imgs_shape[0],imgs_shape[1],1) if(imgs_shape) else (X.shape[1],X.shape[2],1) 
+#    model=cnn.make_model(imgs_shape)
+#    model.fit(X,y,epochs=n_epochs,batch_size=8)#64)
+#    if(out_path):
+#        model.save(out_path)
 
 if __name__=="__main__":
     in_path="../../box"
     out_path="dataset"
 #    data.make_dir(out_path)
-    make_dataset(in_path,out_path)
+#    make_dataset(in_path,out_path)
+    train_model("dataset",out_path=None)
